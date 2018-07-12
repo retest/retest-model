ece951e84d28f913342ea4228e2d9bbf8d580869
Jeremias Roessler <roessler@retest.de>
2018-07-12 09:30:27 +0200

    Adapt tests to changes

--- src/test/java/de/retest/ui/PathTest.java
+++ src/test/java/de/retest/ui/PathTest.java
@@ -21,21 +21,21 @@ public class PathTest {
 
 	@Test
 	public void string_representation_of_simple_path() throws Exception {
-		assertThat( Path.path( ELEMENT_0 ).toString() ).isEqualTo( "elem_0" );
-		assertThat( Path.fromString( "elem_0" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
+		assertThat( Path.path( ELEMENT_0 ).toString() ).isEqualTo( "elem[0]" );
+		assertThat( Path.fromString( "elem[0]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
 	}
 
 	@Test
 	public void string_representation_of_nested_path_with_empty_path() throws Exception {
 		assertThat( Path.path( EMPTY_PATH, EMPTY_ELEMENT ).toString() ).isEqualTo( "/" );
-		assertThat( Path.path( EMPTY_PATH, ELEMENT_0 ).toString() ).isEqualTo( "/elem_0" );
+		assertThat( Path.path( EMPTY_PATH, ELEMENT_0 ).toString() ).isEqualTo( "/elem[0]" );
 		assertThat( Path.fromString( "/" ) ).isEqualTo( Path.path( EMPTY_PATH, EMPTY_ELEMENT ) );
-		assertThat( Path.fromString( "/elem_0" ) ).isEqualTo( Path.path( EMPTY_PATH, ELEMENT_0 ) );
+		assertThat( Path.fromString( "/elem[0]" ) ).isEqualTo( Path.path( EMPTY_PATH, ELEMENT_0 ) );
 	}
 
 	@Test
 	public void string_representation_of_nested_parent_path() throws Exception {
-		assertThat( Path.path( Path.path( ELEMENT_0 ), ELEMENT_1 ).toString() ).isEqualTo( "elem_0/leleme_1" );
+		assertThat( Path.path( Path.path( ELEMENT_0 ), ELEMENT_1 ).toString() ).isEqualTo( "elem[0]/leleme[1]" );
 	}
 
 	@Test
@@ -44,8 +44,8 @@ public class PathTest {
 		final Path path1 = Path.path( path0, ELEMENT_1 );
 		final Path path2 = Path.path( path1, ELEMENT_2 );
 		final Path path3 = Path.path( path2, ELEMENT_3 );
-		assertThat( path3.toString() ).isEqualTo( "elem_0/leleme_1/bubi/muma_3" );
-		assertThat( Path.fromString( "elem_0/leleme_1/bubi/muma_3" ) ).isEqualTo( path3 );
+		assertThat( path3.toString() ).isEqualTo( "elem[0]/leleme[1]/bubi/muma[3]" );
+		assertThat( Path.fromString( "elem[0]/leleme[1]/bubi/muma[3]" ) ).isEqualTo( path3 );
 	}
 
 	@Test
