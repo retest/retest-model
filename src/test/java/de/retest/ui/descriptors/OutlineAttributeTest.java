598ecdd4d50fee0afbd887d6249d33be80dcddc4
Jeremias Roessler <roessler@retest.de>
2018-08-02 11:26:41 +0200

    Add OutlineAttribute from retest-sut-api

---
+++ src/test/java/de/retest/ui/descriptors/OutlineAttributeTest.java
@@ -0,0 +1,61 @@
+package de.retest.ui.descriptors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.awt.Rectangle;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXB;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OutlineAttributeTest {
+
+	OutlineAttribute attribute;
+	Rectangle outline;
+
+	@Before
+	public void setUp() throws Exception {
+		outline = new Rectangle( 1, 2, 3, 4 );
+		attribute = new OutlineAttribute( outline );
+	}
+
+	@Test
+	public void getValue_should_return_equal_rectangle() throws Exception {
+		assertThat( attribute.getValue() ).isEqualTo( outline );
+	}
+
+	@Test
+	public void getValue_roundtrip_should_return_equal_rectangle() throws Exception {
+		final StringWriter writer = new StringWriter();
+		JAXB.marshal( attribute, writer );
+		final StringReader xml = new StringReader( writer.getBuffer().toString() );
+		final OutlineAttribute roundTrip = JAXB.unmarshal( xml, OutlineAttribute.class );
+
+		assertThat( roundTrip.getValue() ).isEqualTo( outline );
+	}
+
+	@Test
+	public void parse_roundtrip_should_return_equal_rectangle() throws Exception {
+		final Rectangle normal = new Rectangle( 1, 10, 100, 0 );
+		assertThat( OutlineAttribute.parse( normal.toString() ) ).isEqualTo( normal );
+
+		final Rectangle negative = new Rectangle( -10, 10, -1, -100 );
+		assertThat( OutlineAttribute.parse( negative.toString() ) ).isEqualTo( negative );
+	}
+
+	@Test
+	public void parse_strange_values_should_work() throws Exception {
+		// assertThat( OutlineAttribute.parse( "java.awt.Rectangle[x=-10,y=10,width=-1,height=-100]" ) )
+
+		assertThat( OutlineAttribute.parse( null ) ).isNull();
+		assertThat( OutlineAttribute.parse( "" ) ).isNull();
+	}
+
+	@Test
+	public void getWeight_should_be_ignored() {
+		assertThat( attribute.getWeight() ).isEqualTo( Attribute.IGNORE_WEIGHT );
+	}
+}
