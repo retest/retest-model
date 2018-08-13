11999bd2dde7a3c0d163d3c2e6da0eff5c080b28
Daniel Kraus <daniel.kraus@retest.de>
2018-08-13 15:24:24 +0200

    Improve Javadoc

--- src/main/java/de/retest/ui/descriptors/RetestIdProvider.java
+++ src/main/java/de/retest/ui/descriptors/RetestIdProvider.java
@@ -1,25 +1,30 @@
 package de.retest.ui.descriptors;
 
 /**
- * This interface can be implemented by customers, to configure how the retest Id is created from given identifying
- * attributes. E.g. some might prefer a combination of class name and text, or id, or path, or some completely random
- * value, like UUID.
+ * This interface can be implemented by customers to configure how the retest ID is created from the given identifying
+ * attributes. For instance, some might prefer a combination of class name and text, or id, or path, or some completely
+ * random value (like a UUID).
  */
 public interface RetestIdProvider {
 
 	public static final String ID_PROVIDER_CONFIGURATION_PROPERTY = "de.retest.RetestIdProvider";
 
 	/**
-	 * Get a retest Id for the given identifying attributes. Ideally, this should be human readable or have some
+	 * Get a retest ID for the given identifying attributes. Ideally, this should be human readable or have some
 	 * intuitive relation to the given identifying attributes.
 	 *
-	 * But it absolutely <em>must</em> be unique within a given state. The {@link #reset()} method is used to in between
-	 * states.
+	 * But it absolutely <em>must</em> be unique within a given state. The {@link #reset()} method is used to reset the
+	 * know IDs in between states.
+	 *
+	 * @param identifyingAttributes
+	 *            the identifying attributes of which the ID should be derived from
+	 *
+	 * @return the generated unique retest ID
 	 */
 	String getRetestId( IdentifyingAttributes identifyingAttributes );
 
 	/**
-	 * Resets the state, such that the same retest id can be used after calling this method.
+	 * Resets the state, such that the same retest ID can be used after calling this method.
 	 */
 	void reset();
 
