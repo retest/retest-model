7a1438967090f19bb0bec450e12536439d920308
Daniel Kraus <daniel.kraus@retest.de>
2018-10-01 16:02:01 +0200

    Move to JUnit 5

--- src/test/java/de/retest/util/RetestIdUtilTest.java
+++ src/test/java/de/retest/util/RetestIdUtilTest.java
@@ -2,19 +2,19 @@ package de.retest.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class RetestIdUtilTest {
+class RetestIdUtilTest {
 
 	@Test
-	public void weired_chars_should_be_removed_when_normalized() {
+	void weired_chars_should_be_removed_when_normalized() {
 		final String rawId = " this. (should) \n be -/ \\ +NORMALIZEDé{}";
 		final String normalizedId = RetestIdUtil.normalize( rawId );
 		assertThat( normalizedId ).isEqualTo( "this_should_be_normalized" );
 	}
 
 	@Test
-	public void too_long_single_word_ids_should_be_cut() {
+	void too_long_single_word_ids_should_be_cut() {
 		final String rawId = "supercalifragilisticexpialidocious";
 		final String cutId = RetestIdUtil.cut( rawId );
 		assertThat( cutId.length() ).isLessThan( 20 );
@@ -22,7 +22,7 @@ public class RetestIdUtilTest {
 	}
 
 	@Test
-	public void too_long_multi_word_ids_should_be_normalized_and_cut_sensibly() {
+	void too_long_multi_word_ids_should_be_normalized_and_cut_sensibly() {
 		final String rawId =
 				"This       is\tsome     very long_sentence, \nthat    could be in a link text, or in some paragraph, and really is to long to be used as id.";
 		final String normalizedAndCutId = RetestIdUtil.normalizeAndCut( rawId );
