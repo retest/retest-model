bb58382e8079ef35299a51018c355b4372f74864
Enes Özel <enes.oezel@retest.de>
2018-11-09 16:23:18 +0100

    Fix tests by using new constructor

--- src/test/java/de/retest/ui/PathTest.java
+++ src/test/java/de/retest/ui/PathTest.java
@@ -6,33 +6,33 @@ import org.junit.Test;
 
 public class PathTest {
 
-	private static final PathElement ELEMENT_0 = new PathElement( "elem", "0" );
-	private static final PathElement ELEMENT_1 = new PathElement( "leleme", "1" );
+	private static final PathElement ELEMENT_0 = new PathElement( "elem", 1 );
+	private static final PathElement ELEMENT_1 = new PathElement( "leleme", 1 );
 	private static final PathElement ELEMENT_2 = new PathElement( "bubi" );
-	private static final PathElement ELEMENT_3 = new PathElement( "muma", "3" );
+	private static final PathElement ELEMENT_3 = new PathElement( "muma", 3 );
 
 	@Test
 	public void string_representation_of_simple_path() throws Exception {
-		assertThat( Path.path( ELEMENT_0 ).toString() ).isEqualTo( "elem[0]" );
-		assertThat( Path.fromString( "elem[0]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
+		assertThat( Path.path( ELEMENT_0 ).toString() ).isEqualTo( "elem[1]" );
+		assertThat( Path.fromString( "elem[1]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
 	}
 
 	@Test
 	public void string_representation_of_nested_path_with_empty_path() throws Exception {
-		assertThat( Path.path( ELEMENT_0 ).toString() ).isEqualTo( "elem[0]" );
-		assertThat( Path.fromString( "elem[0]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
+		assertThat( Path.path( ELEMENT_0 ).toString() ).isEqualTo( "elem[1]" );
+		assertThat( Path.fromString( "elem[1]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
 	}
 
 	@Test
 	public void no_slash_before_root() {
-		assertThat( Path.fromString( "/elem[0]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
-		assertThat( Path.fromString( "//elem[0]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
-		assertThat( Path.fromString( "///elem[0]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
+		assertThat( Path.fromString( "/elem[1]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
+		assertThat( Path.fromString( "//elem[1]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
+		assertThat( Path.fromString( "///elem[1]" ) ).isEqualTo( Path.path( ELEMENT_0 ) );
 	}
 
 	@Test
 	public void string_representation_of_nested_parent_path() throws Exception {
-		assertThat( Path.path( Path.path( ELEMENT_0 ), ELEMENT_1 ).toString() ).isEqualTo( "elem[0]/leleme[1]" );
+		assertThat( Path.path( Path.path( ELEMENT_0 ), ELEMENT_1 ).toString() ).isEqualTo( "elem[1]/leleme[1]" );
 	}
 
 	@Test
@@ -41,8 +41,8 @@ public class PathTest {
 		final Path path1 = Path.path( path0, ELEMENT_1 );
 		final Path path2 = Path.path( path1, ELEMENT_2 );
 		final Path path3 = Path.path( path2, ELEMENT_3 );
-		assertThat( path3.toString() ).isEqualTo( "elem[0]/leleme[1]/bubi/muma[3]" );
-		assertThat( Path.fromString( "elem[0]/leleme[1]/bubi/muma[3]" ) ).isEqualTo( path3 );
+		assertThat( path3.toString() ).isEqualTo( "elem[1]/leleme[1]/bubi[1]/muma[3]" );
+		assertThat( Path.fromString( "elem[1]/leleme[1]/bubi[1]/muma[3]" ) ).isEqualTo( path3 );
 	}
 
 	@Test
